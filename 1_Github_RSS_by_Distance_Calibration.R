7809c5da9bce926d4bf0b6e363a21fe567b334c7
jess <galaxycoff@gmail.com>
2024-09-27 13:36:15 -0400

    simplified

--- 1_Github_RSS_by_Distance_Calibration.R
+++ 1_Github_RSS_by_Distance_Calibration.R
@@ -13,17 +13,14 @@ options(digits=9)
 outpath <- "/home/jess/Documents/workshop/multilateration/"
 
 ## Bring in functions 
-setwd(working.directory)
 source("R/Functions_Paxton-CTTUpdate.R")
 
 #re-sample data for calibration 
-#create time window by reducing location precision
-#or can input data with TestId column (user-defined window)
-
-mytest <- read.csv("~/Downloads/calibration_2023_8_3_all.csv")
+#create time window by reducing location precision or can input data with TestId column (user-defined window)
+mytest <- read.csv("calibration_2023_8_3_all.csv")
 mytest$Time <- as.POSIXct(mytest$time_utc, tz="UTC")
 
-con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows.duckdb", read_only = TRUE)
+con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/AOS2024workshop_Meadows/workshop/meadows.duckdb", read_only = TRUE)
 
 testdata <- tbl(con, "raw") |> #tbl(con, "blu") |>
   filter(time >= as.Date("2023-07-31") & time <= as.Date("2023-10-31")) |>
@@ -41,73 +38,12 @@ DBI::dbDisconnect(con)
 
 nodes <- node_file(nodehealth)
 
-combined.data <- data.setup(mytest, testdata, nodes, tag_col="tag_id", tagid="072A6633", time_col="Time",timezone="UTC",x="lon",y="lat", loc_precision=6, fileloc="/home/jess/Documents/radio_projects/data/radio_projects/meadows/meadows.duckdb", filetype="raw") #, loc_precision=4
-## Bring in 3 Needed files - Test Information, RSS values, and Node Information - change file names in " " as needed
-#test.info_k <- read.csv("Test.Info_Example.csv", header = T)
-
-#test <- test[-which(test$Tag.Type=="userLocation"),]
-
-
-
-#testdata$TestId <- 0L
-
-#str(test.info) # check that data imported properly
-
-#beep.dat <- readRDS("BeepData_Example.rds") 
-#str(beep.dat) # check that data imported properl
-
-#nodes <- read.csv("Nodes_Example.csv", header = T)
-#str(nodes)
-
-
-
-# rearrange data
-
-
-
-## Combine distances with summary data 
-
-
-
 #################################################################
 #   Step 1
 # Run function to isolate raw RSS data from a node network that
 # is associated with test data  
 #################################################################
-
-    ## Variables to define for function
-        ## TEST.TYPE = category indicating the type of dataset
-            ## "Calibration" - test dataset used for calibrating the relationship between RSS and Distance (purpose of this R script)
-            ## "LocError" - test data used to determine localization error associated with RSS-based localization estimates for a node network
-        ## DATE.FORMAT = format of the date column in Test.Info.csv file using R standard date expressions (e.g., "%m-%d-%y", "%Y-%m-%d")
-        ## TIMEZONE = Time zone where data was collected, use grep("<insert location name here>", OlsonNames(), value=TRUE) to find valid time zone name
-
-
-    ## Output in R environment
-        ## combined.data - dataframe that contains the average RSS values for a given node associated with each unique test
-            ## Columns:
-                ## NodeId - unique identifier of the specified node
-                ## TestId - unique identifier of the specified test
-                ## avgRSS - average RSS value across a 3-min time period for the given node and test
-                ## sdRSS - standard deviation of RSS values across the 3-min time period for the given node and test
-                ## distance - true distance (in meters) between the specified node and test location
-                ## NodeUTMx - Easting location of the specified node
-                ## NodeUTMy - Northing location of the specified node
-                ## TestUTMx - Easting location of the specified test
-                ## TestUTMy - Northing location of the specified test
-
-    ## Output saved
-        ## Calibration_Dataset.csv - .csv file of the dataframe 'combine.data' saved in the folder specified by the outpath
-
-
-##* Define Variables - replace values below with user specified values **## 
-#TEST.TYPE <- "Calibration"
-#DATE.FORMAT <- "%m/%d/%y"
-#TIME.ZONE <- "Pacific/Guam"
-
-
-# Combine RSS data from a node network with test information into a dataframe
-#combined.data <- data.setup(TEST.TYPE, DATE.FORMAT, TIME.ZONE)
+combined.data <- data.setup(mytest, testdata, nodes, tag_col="tag_id", tagid="072A6633", time_col="Time",timezone="UTC",x="lon",y="lat", loc_precision=6, fileloc="/home/jess/Documents/radio_projects/data/radio_projects/meadows/meadows.duckdb", filetype="raw")
 
 
 ##########################################################
@@ -115,15 +51,10 @@ combined.data <- data.setup(mytest, testdata, nodes, tag_col="tag_id", tagid="07
 # Exponential Decay Function to Examine Relationship 
 # between distance and Tag RSS values 
 ##########################################################
-
-
 ## Visualize data
-
   # Plot of the relationship between RSS and distance
 ggplot(data = combined.data, aes(x = distance, y = avgRSS, color = node_id)) +
   geom_point(size = 2)
-
-
 
 ## Preliminary Exponential Decay Model to determine starting values for the final model 
 
@@ -131,7 +62,6 @@ ggplot(data = combined.data, aes(x = distance, y = avgRSS, color = node_id)) +
     # Asym - horizontal asymptote (when large values ) - y values decay to this value 
     # R0 - numeric value when avgRSS (i.e., response variable) = 0 
     # lrc - natural logarithm of the rate constant (rate of decay)
-
 
   # Preliminary Model
 #combined.data <- combined.data[!is.na(combined.data$distance),]
@@ -141,9 +71,6 @@ summary(exp.mod)
   # rate of decay
 exp(coef(exp.mod)[["lrc"]])
 
-
-
-
 ## Final Exponential Decay Model with user provided self-starting values 
 ## based on visualization of the data and values in the Preliminary Model Output
 
@@ -151,7 +78,6 @@ exp(coef(exp.mod)[["lrc"]])
       # a = intercept
       # S = decay factor
       # K = horizontal asymptote
-
 
 ##  ***** Variables to define for final model below - replace values below with values from exp.mod ****  ## 
 a <- coef(exp.mod)[["R0"]]
@@ -167,40 +93,27 @@ summary(nls.mod)
     # **** you will use these coefficient values to estimate distance in Github_TestDataset_LocalizationError.R and Github_Simulations.R scripts *****
 coef(nls.mod)
 
-
 ## Check the fit of the model and get predicted values
-
   # Get residuals and fit of model and add variables to main table
 combined.data$E <- residuals(nls.mod)
 combined.data$fit <- fitted(nls.mod)
 
   # Plot residuals by fit or distance
-ggplot(combined.data, aes(x = distance, y = E, color = node_id)) +
-         geom_point(size = 2)
-ggplot(combined.data, aes(x = fit, y = E, color = node_id)) +
-  geom_point(size = 2)
+#ggplot(combined.data, aes(x = distance, y = E, color = node_id)) +
+#         geom_point(size = 2)
+#ggplot(combined.data, aes(x = fit, y = E, color = node_id)) +
+#  geom_point(size = 2)
 
   # Get model predictions
 combined.data$pred <- predict(nls.mod)
 
-
-## Save Final Dataset with Residuals and Predictions
-#write.csv(combined.data, paste0(outpath, "Calibration_Dataset_withResiduals_Predictions.csv"),
-#          row.names = F)
-
-
 ## Plot with predicted line
-
-#pdf(paste0(outpath, "Relationship_Distance~RSSI.pdf"), width = 8, height = 6)
-
-ggplot(combined.data, aes(x = distance, y = avgRSS)) + 
+ggplot(combined.data, aes(x = distance, y = avgRSS, color=node_id)) + 
   geom_point() +
   geom_line(aes(y = pred), color = "#689FBB", lwd = 1.25) +
   scale_y_continuous(name = "RSS (dB)") +
   scale_x_continuous(name = "Distance (m)") +
   theme_classic()
-
-#dev.off()
 
 combined.data <- estimate.distance(combined.data, unname(coef(nls.mod)[3]), unname(coef(nls.mod)[1]), unname(coef(nls.mod)[2]))
 no.filters <- trilateration.TestData.NoFilter(combined.data)
--- workshop_API.R
+++ workshop_API.R
@@ -8,11 +8,11 @@ outpath <- "~/Documents/workshop" #where your downloaded files are to go
 con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows_test.duckdb", read_only = FALSE)
 my_token <- "c2ed5f935e9b9d4c2e031f8a96277317b7502d989add5947656dbfbeee7082c5"
 ################
-get_my_data(my_token, outpath, con, myproject=myproject, begin=as.Date("2023-09-01"), end=as.Date("2023-09-07"), filetypes=c("raw", "node_health"))
+get_my_data(my_token, outpath, con, myproject=myproject, begin=as.Date("2023-09-01"), end=as.Date("2023-09-30"), filetypes=c("raw", "node_health"))
 update_db(con, outpath, myproject)
 DBI::dbDisconnect(con)
 
 time_elapse <- Sys.time() - start
 print(time_elapse)
 
-#raw <- tbl(con, "node_health") |> collect()
+#raw <- tbl(con, "raw") |> collect()
