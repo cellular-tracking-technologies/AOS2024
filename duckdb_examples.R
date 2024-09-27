360f4667dd22250a1ab1a19971d469b658533b2e
Jessica Gorzo <galaxycoff@gmail.com>
2024-09-27 10:25:25 -0400

    smaller subset

--- duckdb_examples.R
+++ duckdb_examples.R
@@ -6,7 +6,7 @@ tagid = c("072A6633","2D4B782D")
 con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows.duckdb", read_only = TRUE)
 
 testdata <- tbl(con, "raw") |> 
-  filter(time >= as.Date("2023-07-31") & time <= as.Date("2023-09-31")) |>
+  filter(time >= as.Date("2023-07-31") & time <= as.Date("2023-08-02")) |>
   filter(tag_id %in% tagid) |>
   collect()
 
--- workshop_API.R
+++ workshop_API.R
@@ -5,9 +5,9 @@ start <- Sys.time()
 ####SETTINGS#####
 myproject <- "Meadows V2" #this is your project name on your CTT account
 outpath <- "~/Documents/workshop" #where your downloaded files are to go 
-con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows.duckdb", read_only = FALSE)
+con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows_test.duckdb", read_only = FALSE)
 ################
-get_my_data(my_token, outpath, con, myproject=myproject, begin=as.Date("2023-07-31"), end=as.Date("2023-10-31"), filetypes=c("raw", "node_health"))
+get_my_data(my_token, outpath, con, myproject=myproject, begin=as.Date("2023-07-31"), end=as.Date("2023-08-31"), filetypes=c("raw", "node_health"))
 update_db(con, outpath, myproject)
 DBI::dbDisconnect(con)
 
