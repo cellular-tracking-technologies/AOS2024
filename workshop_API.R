6c811807b6e39793e9e1fb195943e47e6f336a7b
jess <galaxycoff@gmail.com>
2024-09-27 11:58:25 -0400

    shorter time window

--- workshop_API.R
+++ workshop_API.R
@@ -6,8 +6,9 @@ start <- Sys.time()
 myproject <- "Meadows V2" #this is your project name on your CTT account
 outpath <- "~/Documents/workshop" #where your downloaded files are to go 
 con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows_test.duckdb", read_only = FALSE)
+my_token <- "c2ed5f935e9b9d4c2e031f8a96277317b7502d989add5947656dbfbeee7082c5"
 ################
-get_my_data(my_token, outpath, con, myproject=myproject, begin=as.Date("2023-07-31"), end=as.Date("2023-08-31"), filetypes=c("raw", "node_health"))
+get_my_data(my_token, outpath, con, myproject=myproject, begin=as.Date("2023-09-01"), end=as.Date("2023-09-07"), filetypes=c("raw", "node_health"))
 update_db(con, outpath, myproject)
 DBI::dbDisconnect(con)
 
