4033f8f771d2679e91a11e415f5b4d4dab90240c
Jess <galaxycoff@gmail.com>
2024-09-26 23:14:16 -0400

    duckdb examples

---
+++ duckdb_examples.R
@@ -0,0 +1,20 @@
+library(duckdb)
+library(dplyr)
+
+tagid = c("072A6633","2D4B782D")
+
+con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows.duckdb", read_only = TRUE)
+
+testdata <- tbl(con, "raw") |> 
+  filter(time >= as.Date("2023-07-31") & time <= as.Date("2023-09-31")) |>
+  filter(tag_id %in% tagid) |>
+  collect()
+
+start_buff = as.Date("2023-08-01", tz="UTC") #start - 2*60*60
+end_buff = as.Date("2023-08-07", tz="UTC") #end + 2*60*60
+
+nodehealth <- tbl(con, "node_health") |>
+  filter(time >= start_buff  & time <= end_buff) |>
+  collect()
+
+DBI::dbDisconnect(con)
