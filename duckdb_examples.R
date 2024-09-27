fedbb18cf8711a084cb0e4d4712df8ece44ae599
jess <galaxycoff@gmail.com>
2024-09-27 12:01:17 -0400

    shorter time window

--- duckdb_examples.R
+++ duckdb_examples.R
@@ -6,12 +6,12 @@ tagid = c("072A6633","2D4B782D")
 con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "/home/jess/Documents/workshop/meadows_test.duckdb", read_only = TRUE)
 
 testdata <- tbl(con, "raw") |> 
-  filter(time >= as.Date("2023-07-31") & time <= as.Date("2023-08-02")) |>
+  filter(time >= as.Date("2023-09-02") & time <= as.Date("2023-09-03")) |>
   filter(tag_id %in% tagid) |>
   collect()
 
-start_buff = as.Date("2023-08-01", tz="UTC") 
-end_buff = as.Date("2023-08-07", tz="UTC")
+start_buff = as.Date("2023-09-01", tz="UTC") 
+end_buff = as.Date("2023-09-03", tz="UTC")
 
 nodehealth <- tbl(con, "node_health") |>
   filter(time >= start_buff  & time <= end_buff) |>
